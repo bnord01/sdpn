98fb2dd584dbd336f89e06e636b11f84ab0eaaba
bnord <b.n@wwu.de>
2011-12-07 18:00:20 +0100

    Fix: Look for methods invoked at to call sites within one method in CGCycleCounter.

--- de.wwu.sdpn.wala/src/main/scala/de/wwu/sdpn/wala/util/CGCycleCounter.scala
+++ de.wwu.sdpn.wala/src/main/scala/de/wwu/sdpn/wala/util/CGCycleCounter.scala
@@ -13,107 +13,111 @@ import scala.collection.JavaConversions._
  * @author Benedikt Nordhoff
  */
 class CGCycleCounter(val cg: CallGraph) {
-    private var inftyNodes: Set[CGNode] = null
-    private var ucNodes: Set[CGNode] = null
+  private var inftyNodes: Set[CGNode] = null
+  private var ucNodes: Set[CGNode] = null
 
-    def getInftyNodes() = {
-        if (inftyNodes != null)
-            inftyNodes
-        else {
-            val infnodes = calcInftyNodes
-            inftyNodes = Set()
-            inftyNodes ++= infnodes
-            inftyNodes
-        }
+  def getInftyNodes() = {
+    if (inftyNodes != null)
+      inftyNodes
+    else {
+      val infnodes = calcInftyNodes
+      inftyNodes = Set()
+      inftyNodes ++= infnodes
+      inftyNodes
     }
-    def getUCNodes() = {
-        if (ucNodes != null)
-            ucNodes
-        else {
-            getInftyNodes()
-            ucNodes = Set()
-            for (x <- cg if (!inftyNodes.contains(x))) {
-                ucNodes += x
+  }
+  def getUCNodes() = {
+    if (ucNodes != null)
+      ucNodes
+    else {
+      getInftyNodes()
+      ucNodes = Set()
+      for (x <- cg if (!inftyNodes.contains(x))) {
+        ucNodes += x
+      }
+      ucNodes
+    }
+  }
+
+  private def calcInftyNodes() = {
+    //TODO Check if two call sites call the same successor
+    import CGCycleCounter.findInftyNodes
+    //find cycles in call graph
+    val cginfty = findInftyNodes(cg, cg.getFakeRootNode)
+    //find cycles within controll flow graphs of non infty nodes
+    for (node <- cg) {
+      if (!cginfty.contains(node)) {
+        if (node.getIR != null) {
+          val cfg = node.getIR.getControlFlowGraph
+          val start = cfg.entry
+          //val bbinfty = findInftyNodes(cfg, start)
+          val bbinfity = new GraphCycleFinder(cfg)
+          bbinfity.solve(null)
+          for (bb <- cfg if bbinfity.inCycle(bb); instr <- bb) {
+            instr match {
+              case call: SSAAbstractInvokeInstruction =>
+                cginfty ++= cg.getPossibleTargets(node, call.getCallSite)
+              case _ =>
             }
-            ucNodes
+          }
         }
+      }
     }
 
-    private def calcInftyNodes() = {
-      	//TODO Check if two call sites call the same successor
-        import CGCycleCounter.findInftyNodes
-        //find cycles in call graph
-        val cginfty = findInftyNodes(cg, cg.getFakeRootNode)
-        //find cycles within controll flow graphs of non infty nodes
-        for (node <- cg) {
-            if (!cginfty.contains(node)) {
-                if (node.getIR != null) {
-                    val cfg = node.getIR.getControlFlowGraph
-                    val start = cfg.entry
-                    //val bbinfty = findInftyNodes(cfg, start)
-                    val bbinfity = new GraphCycleFinder(cfg)
-                    bbinfity.solve(null)
-                    for (bb <- cfg if bbinfity.inCycle(bb); instr <- bb) {
-                        instr match {
-                            case call: SSAAbstractInvokeInstruction =>
-                                cginfty ++= cg.getPossibleTargets(node, call.getCallSite)
-                            case _ =>
-                        }
-                    }
-                }
-            }
+    val cgtoVisit = Queue[CGNode]()
+    cgtoVisit ++= cginfty
+    while (!cgtoVisit.isEmpty) {
+      val node = cgtoVisit.dequeue
+      for (cnode <- cg.getSuccNodes(node)) {
+        if (!cginfty.contains(cnode)) {
+          cginfty += cnode
+          cgtoVisit += cnode
         }
-
-        val cgtoVisit = Queue[CGNode]()
-        cgtoVisit ++= cginfty
-        while (!cgtoVisit.isEmpty) {
-            val node = cgtoVisit.dequeue
-            for (cnode <- cg.getSuccNodes(node)) {
-                if (!cginfty.contains(cnode)) {
-                    cginfty += cnode
-                    cgtoVisit += cnode
-                }
-            }
-        }
-        cginfty
+      }
     }
+    cginfty
+  }
 
 }
 
 /**
- * Helper to find nodes to which there exists more than one path from start 
+ * Helper to find nodes to which there exists more than one path from start
  * @author Benedikt Nordhoff
  */
 object CGCycleCounter {
-    def findInftyNodes[T](graph: Graph[T], start: T) = {
-        val visited = HashSet[T](start)
-        val infty = HashSet[T]()
-        val toVisit = Queue[T](start)
+  def findInftyNodes(cg: CallGraph, start: CGNode) = {
+    val visited = HashSet[CGNode](start)
+    val infty = HashSet[CGNode]()
+    val childrenToVisit = Queue[CGNode](start)
 
-        //Find cycles in Graph
-        while (!toVisit.isEmpty) {
-            addChildren(toVisit.dequeue)
-        }
-        //visit all infty nodes again adding 
-        //everything to infty what's reachable from there 
-        toVisit ++= infty
-        while (!toVisit.isEmpty) {
-            addChildren(toVisit.dequeue)
-        }
-
-        def addChildren(node: T) {
-            val it: Iterator[T] = graph.getSuccNodes(node)
-            while (it.hasNext) {
-                val cnode = it.next
-                if (visited.contains(cnode)) {
-                    infty += cnode
-                } else {
-                    toVisit += cnode
-                    visited += cnode
-                }
-            }
-        }
-
-        infty //return infty set    
+    //Find cycles in Graph
+    while (!childrenToVisit.isEmpty) {
+      addChildren(childrenToVisit.dequeue)
     }
+    //visit all infty nodes again adding 
+    //everything to infty what's reachable from there 
+    childrenToVisit ++= infty
+    while (!childrenToVisit.isEmpty) {
+      addChildren(childrenToVisit.dequeue)
+    }
+
+    def addChildren(node: CGNode) {
+      val it: Iterator[CGNode] = cg.getSuccNodes(node)
+      while (it.hasNext) {
+        val cnode = it.next
+        if (visited.contains(cnode)) {
+          infty += cnode
+        } else if (cg.getPossibleSites(node, cnode).size > 1) {
+          visited += cnode
+          infty += cnode
+          childrenToVisit += cnode
+        } else {
+          childrenToVisit += cnode
+          visited += cnode
+        }
+      }
+    }
+
+    infty //return infty set    
+  }
 }
