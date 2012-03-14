d78ed441eaef8d46ecab3d64e8522219e0a8c131
bnord <bnord01@gmail.com>
2012-03-14 11:25:14 +0100

    Updated Interprolog to version 2.1.2a4

--- de.wwu.sdpn.core/lib/installInterprologMaven.sh
+++ de.wwu.sdpn.core/lib/installInterprologMaven.sh
@@ -1,2 +1,2 @@
 mvn install:install-file -Dfile=interprolog.jar -DgroupId=com.declarativa \
-    -DartifactId=interprolog -Dversion=2.1.2a -Dpackaging=jar
+    -DartifactId=interprolog -Dversion=2.1.2a4 -Dpackaging=jar
