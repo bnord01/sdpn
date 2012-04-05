af4d41a92194085337a66132ac591b2f676fbaf5
bnord <bnord01@gmail.com>
2012-04-05 12:10:23 +0200

    Generating scripts for xsb/datalog comparison.

--- de.wwu.sdpn.wala/src/test/scala/de/wwu/sdpn/tests/seidl/SeidlDatalogTest.scala
+++ de.wwu.sdpn.wala/src/test/scala/de/wwu/sdpn/tests/seidl/SeidlDatalogTest.scala
@@ -23,10 +23,14 @@ import com.ibm.wala.ipa.callgraph.CGNode
 import de.wwu.sdpn.core.ta.datalog.reachability.MDPN2TA
 import de.wwu.sdpn.core.ta.datalog.reachability.SingleSetConflictTA
 import de.wwu.sdpn.core.ta.datalog.IntersectionEmptinessCheck
+import de.wwu.sdpn.core.ta.xsb.reachability.{ MDPN2TA => XMDPN2TA }
+import de.wwu.sdpn.core.ta.xsb.reachability.{ SingleSetConflictTA => XSSCTA }
+import de.wwu.sdpn.core.ta.xsb.{ IntersectionEmptinessCheck => XIEC }
 import java.io.File
 import java.io.BufferedWriter
 import java.io.FileOutputStream
 import java.io.FileWriter
+import java.io.PrintWriter
 
 object SeidlDatalogTest {
 
@@ -34,6 +38,7 @@ object SeidlDatalogTest {
 
     @BeforeClass
     def setUp() {
+
         for (i <- 1 to 7) {
             val (cg, pa) = SimpleAnalyses.getCGandPAfromCP(SDPNTestProps.get.classPath, "Lbnord/unittests/simpleAnalyses/BSP0" + i)
             val mr = StringStuff.makeMethodReference("bnord.unittests.simpleAnalyses.BSP0" + i + ".excludeMe()V")
@@ -61,126 +66,73 @@ object SeidlDatalogTest {
 
 class SeidlDatalogTest {
     import SeidlDatalogTest.stuff
+
     @Test
-    def testPrintCGandStackSymbols() {
-        val (cg, pa, mr) = stuff(1)
-        println("Number of CGNodes:\t" + cg.getNumberOfNodes())
-        println("Identified StackSymbols:\t" + getStackSymbols(cg, mr))
+    def testBenchmarkPrograms() {
+        val progs = List(
+            "Plain",
+            "CrossCallsTwo",
+            "CrossCallsThree",
+            "CrossCallsFour",
+            "CrossCallsFive",
+            "CrossCallsSix",
+            "CrossCallsSeven",
+            "CrossCallsEight",
+            "CrossCallsNine",
+            "Println"
+        )
+
+        progs foreach { name =>
+            val (cg, pa) = SimpleAnalyses.getCGandPAfromCP(SDPNTestProps.get.classPath, "Lbnord/benchmark/ssr/" + name)
+            val mr = StringStuff.makeMethodReference("bnord.benchmark.ssr." + name + ".excludeMe()V")
+
+            printCheck("sliced_" + name, (cg, pa, mr), true)
+            printCheck("unsliced_" + name, (cg, pa, mr), false)
+        }
+
+        val tempDir = SDPNProps.get.tempDir
+        val f = new File(tempDir, "runAll.sh")
+        val out = new PrintWriter(new BufferedWriter(new FileWriter(f)))
+        try {
+            for (
+                pref <- List("sliced", "unsliced");
+                name <- progs
+            ) {
+                out.println("echo '>>>>>>   Running XSB on:        " + pref + " " + name + "    <<<<<<'")
+                out.println("echo ' lines:'")
+                out.printf("wc -l %s_%s_xsb.P %n", pref, name)
+                out.printf("time xsb --quietload --nobanner %s_%s_xsb %n", pref, name)
+                out.println("echo '>>>>>>   Running BOUND XSB on:  " + pref + " " + name + "    <<<<<<'")
+                out.println("echo ' lines:'")
+                out.printf("wc -l %s_%s_bound_xsb.P %n", pref, name)
+                out.printf("time xsb --quietload --nobanner %s_%s_bound_xsb %n", pref, name)
+                out.println("echo '>>>>>>   Running FSOLVE on :    " + pref + " " + name + "    <<<<<<'")
+                out.println("echo ' lines:'")
+                out.printf("wc -l %s_%s.P %n", pref, name)
+                out.printf("time fsolve %s_%s.P %n", pref, name)
+
+            }
+        } finally {
+            out.close()
+        }
     }
 
-//    @Test
-//    def testUnslicedLockInsensSSR1() {
-//        
-//        val (cg, pa, mr) = stuff(1)
-//        assertFalse("There should be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), null, false))
-//    }
+    //    @Test
+    //    def testUnslicedLockInsensSSR1() {
+    //        
+    //        val (cg, pa, mr) = stuff(1)
+    //        assertFalse("There should be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), null, false))
+    //    }
     @Test
     def testSlicedLockInsensSSR1() {
-        printCheck("ssr1_sliced_true",stuff(1),true)
+        printCheck("ssr1_sliced_true", stuff(1), true)
     }
     @Test
     def testUnslicedLockInsensSSR1() {
-        printCheck("ssr1_unsliced_true",stuff(1),false)
-        
-    }
-//
-//    @Test
-//    def testSlicedLockInsensWitnessTSR1() {
-//        val (cg, pa, mr) = stuff(1)
-//        val nodes = cg.getNodes(mr)
-//        SimpleAnalyses.runWitnessTSRCheck(cg, pa, getStackSymbols(cg, mr), getStackSymbols(cg, mr), nodes, false) match {
-//            case None    => fail
-//            case Some(x) => println("Witness: " + x)
-//        }
-//    }
-//
-//    @Test
-//    def testSlicedLockSensSSR1() {
-//        val (cg, pa, mr) = stuff(2)
-//        val nodes = cg.getNodes(mr)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), nodes, true))
-//    }
-//    @Test
-//    def testStdLibSlicedLockSensSSR1() {
-//        val (cg, pa, mr) = stuff(2)
-//        val nodes = cg.getNodes(mr)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runStdLibSSRCheck(cg, pa, getStackSymbols(cg, mr), nodes))
-//    }
-//
-//    @Test
-//    def testSlicedLockSensSSR1LS() {
-//        val (cg, pa, mr) = stuff(2)
-//        val nodes = cg.getNodes(mr)
-//        var locks = SimpleAnalyses.getPossibleLocks(cg, pa)
-//        locks = SimpleAnalyses.filterByClassLoader(locks)
-//        println("PossibleLocks: " + locks)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), nodes, locks))
-//    }
-//
-//    @Test
-//    def testSlicedLockSensSSR2() {
-//        val (cg, pa, mr) = stuff(3)
-//        val nodes = cg.getNodes(mr)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), nodes, true))
-//    }
-//    @Test
-//    def testSlicedLockSensSSR7() {
-//        val (cg, pa, mr) = stuff(7)
-//        val nodes = cg.getNodes(mr)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), nodes, true))
-//    }
-//    @Test
-//    def testSlicedStdLibLockSensSSR2() {
-//        val (cg, pa, mr) = stuff(3)
-//        val nodes = cg.getNodes(mr)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runStdLibSSRCheck(cg, pa, getStackSymbols(cg, mr), nodes))
-//    }
-//
-//    @Test
-//    def testSlicedLockSensSSR2LS() {
-//        val (cg, pa, mr) = stuff(3)
-//        val nodes = cg.getNodes(mr)
-//        var locks = SimpleAnalyses.getPossibleLocks(cg, pa)
-//        locks = SimpleAnalyses.filterByClassLoader(locks)
-//        println("PossibleLocks: " + locks)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), nodes, locks))
-//    }
-//
-//    @Test
-//    def testSlicedWitnessLockSensTSR2() {
-//        val (cg, pa, mr) = stuff(3)
-//        val nodes = cg.getNodes(mr)
-//        val res = SimpleAnalyses.runWitnessTSRCheck(cg, pa, getStackSymbols(cg, mr), getStackSymbols(cg, mr), nodes, true)
-//
-//        assertTrue("There shouldn't be an Conflict", res == None)
-//    }
-//
-//    @Test
-//    def testSlicedWitnessLockSensTSR3() {
-//        val (cg, pa, mr) = stuff(5)
-//        val nodes = cg.getNodes(mr)
-//        val res = SimpleAnalyses.runWitnessTSRCheck(cg, pa, getStackSymbols(cg, mr), getStackSymbols(cg, mr), nodes, true)
-//
-//        assertFalse("There should be an Conflict", res == None)
-//    }
-//
-//    @Test
-//    def testStdLibUnslicedLockSensSR2() {
-//        val (cg, pa, mr) = stuff(2)
-//        assertTrue("There shouldn't be an Conflict", SimpleAnalyses.runStdLibSSRCheck(cg, pa, getStackSymbols(cg, mr), null))
-//    }
-//
-//    @Test
-//    def testUnslicedLockSensSR6() {
-//        val (cg, pa, mr) = stuff(6)
-//        assertFalse("There should be an Conflict", SimpleAnalyses.runSSRCheck(cg, pa, getStackSymbols(cg, mr), null, true))
-//    }
-//    @Test
-//    def testStdLibUnslicedLockSensSR6() {
-//        val (cg, pa, mr) = stuff(6)
-//        assertFalse("There should be an Conflict", SimpleAnalyses.runStdLibSSRCheck(cg, pa, getStackSymbols(cg, mr), null))
-//    }
+        printCheck("ssr1_unsliced_true", stuff(1), false)
 
+    }
+    
     def printCheck(name: String, input: (CallGraph, PointerAnalysis, MethodReference), slice: Boolean) {
         val (cg, pa, mr) = input
         val stackSyms = getStackSymbols(cg, mr)
@@ -196,6 +148,28 @@ class SeidlDatalogTest {
             out.write(check.emptiness)
         } finally {
             out.close()
+        }
+
+        val xfta = new XMDPN2TA(dpn)
+        val xcta = new XSSCTA("conflict", stackSyms)
+        val xcheck = new XIEC(xfta, xcta)
+        val f2 = new File(tempDir, name + "_xsb.P")
+        val out2 = new BufferedWriter(new FileWriter(f2))
+        try {
+            out2.write(xcheck.emptiness)
+            out2.write("\n")
+            out2.write(":- " + xcheck.name + "_runCheck.")
+        } finally {
+            out2.close()
+        }
+
+        val f3 = new File(tempDir, name + "_bound_xsb.P")
+        val out3 = new BufferedWriter(new FileWriter(f3))
+        try {
+            out3.write(check.emptiness.replace("?-",
+                ":- table (name_ne/2).\n:-".replace("name", check.name)))
+        } finally {
+            out3.close()
         }
     }
 
