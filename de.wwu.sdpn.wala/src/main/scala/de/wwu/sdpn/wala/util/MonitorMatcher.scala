840fe643410a65b98ef88754d3478226c0d9a626
bnord <bnord01@gmail.com>
2012-05-14 11:33:57 +0200

    Bugfix in MonitorMatcher! Use SSACFG#BasicBlock instead of ISSABasicBlock!

--- de.wwu.sdpn.wala/src/main/scala/de/wwu/sdpn/wala/util/MonitorMatcher.scala
+++ de.wwu.sdpn.wala/src/main/scala/de/wwu/sdpn/wala/util/MonitorMatcher.scala
@@ -12,16 +12,16 @@ import scala.collection.Set
 /**
  * A helper used at DPN construction
  * mapping monitorenter instructions to the corresponding monitorexits
- * 
+ *
  * @author Benedikt Nordhoff
  */
-object MonitorMatcher {   
+object MonitorMatcher {
 
-    def getExits(cfg: SSACFG, bbNr: Int): Set[(ISSABasicBlock,Int)] = {
+    def getExits(cfg: SSACFG, bbNr: Int): Set[(SSACFG#BasicBlock, Int)] = {
         //TODO: This assumes there are no cycles except for monitorExits
         //TODO: does it?
         val bb = cfg.getBasicBlock(bbNr)
-        val visited = scala.collection.mutable.Set[ISSABasicBlock]()
+        val visited = scala.collection.mutable.Set[SSACFG#BasicBlock]()
         val vnr = getMI(bb) match {
             case Some(enter) =>
                 if (!enter.isMonitorEnter)
@@ -31,14 +31,23 @@ object MonitorMatcher {
                 throw new IllegalArgumentException("bb contains no Monitor Enter as first instruction!")
         }
 
-        def getExits(bb: ISSABasicBlock, counter: Int): Iterable[(ISSABasicBlock,Int)] = {
+        def getExits(bb: SSACFG#BasicBlock, counter: Int): Iterable[(SSACFG#BasicBlock, Int)] = {
             if (visited(bb)) {
                 return Iterable()
             }
             visited += bb
 
-            import cfg.{ getNormalSuccessors => nSucc, getExceptionalSuccessors => eSucc }
-
+            
+            def nSucc(bb:SSACFG#BasicBlock) = cfg.getNormalSuccessors(bb).map({
+                case b: SSACFG#BasicBlock => b
+                case x => throw new IllegalArgumentException ("Didn't expect non SSACFG#BasicBlock in SSACFG! " + x)
+            })
+            
+            def eSucc(bb:SSACFG#BasicBlock) = cfg.getExceptionalSuccessors(bb)map({
+                case b: SSACFG#BasicBlock => b
+                case x => throw new IllegalArgumentException ("Didn't expect non SSACFG#BasicBlock in SSACFG! " + x)
+            })
+            
             getMI(bb) match {
                 case Some(monitorInstr) if (monitorInstr.getRef == vnr) =>
                     if (monitorInstr.isMonitorEnter) {
@@ -50,9 +59,9 @@ object MonitorMatcher {
                                 eSucc(bb).flatMap(getExits(_, counter))
                         else {
                             val index = findMIIndex(bb)
-                            return eSucc(bb).flatMap(getExits(_, counter)) += ((bb,index))
+                            return eSucc(bb).flatMap(getExits(_, counter)) += ((bb, index))
                         }
-                            
+
                     }
                 case _ =>
                     return nSucc(bb).flatMap(getExits(_, counter)) ++
@@ -63,33 +72,35 @@ object MonitorMatcher {
         return Set() ++ getExits(bb, -1);
     }
 
-    def getMI(bb: ISSABasicBlock): Option[SSAMonitorInstruction] = {
-         // if this is removed need to check findMIIndex
-        assert(bb.filter(_.isInstanceOf[SSAMonitorInstruction]).size < 2,"Can't handle more than 1 monitor instruction per basic block.")
+    def getMI(bb: SSACFG#BasicBlock): Option[SSAMonitorInstruction] = {
+        // if this is removed need to check findMIIndex
+        assert(bb.filter(_.isInstanceOf[SSAMonitorInstruction]).size < 2, "Can't handle more than 1 monitor instruction per basic block.")
         bb.find(_.isInstanceOf[SSAMonitorInstruction]).map(_.asInstanceOf[SSAMonitorInstruction])
     }
-    
-    def findMIIndex(bb: ISSABasicBlock) : Int = {
+
+    def findMIIndex(bb: SSACFG#BasicBlock): Int = {
         var index = 0
-        bb.iteratePhis().foreach(_=> index += 1)
-        if(bb.isCatchBlock())
+        bb.iteratePhis().foreach(_ => index += 1)
+        if (bb.isCatchBlock())
             index += 1
-        for (ins <- bb) {
+        for (ins <- bb.iterateNormalInstructions) {
             if (ins.isInstanceOf[SSAMonitorInstruction]) // assuming there is only one as guaranteed by getMI 
                 return index
             index += 1
         }
-            throw new IllegalArgumentException("Basic block didn't contain any SSAMonitorInstruction " + bb)        
+        throw new IllegalArgumentException("Basic block didn't contain any SSAMonitorInstruction " + bb)
     }
 
-    def getEnters(cfg: SSACFG): Set[ISSABasicBlock] = {
-        var res = Set[ISSABasicBlock]()
+    def getEnters(cfg: SSACFG): Set[SSACFG#BasicBlock] = {
+        var res = Set[SSACFG#BasicBlock]()
 
         for (bb <- cfg.iterator) {
-            getMI(bb) match {
+            assert(bb.isInstanceOf[SSACFG#BasicBlock], "Didn't expext non SSACFG#BasicBlock in SSACFG!" + bb)
+            val bs = bb.asInstanceOf[SSACFG#BasicBlock]
+            getMI(bs) match {
                 case Some(mi) =>
                     if (mi.isMonitorEnter)
-                        res += bb
+                        res += bs
                 case _ =>
             }
         }
